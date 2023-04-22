2ad9efa095265471240cfa51ed86dfbb95f09070
Alex Astudillo <astudilloalex1@gmail.com>
2023-04-22 17:46:19 -0500

    Add rail

--- lib/ui/pages/home/home_page.dart
+++ lib/ui/pages/home/home_page.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:simple_accounting_offline/ui/pages/home/cubit/home_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Row(
+        children: [
+          NavigationRail(
+            extended: context.watch<HomeCubit>().state.extendedRail,
+            destinations: [
+              NavigationRailDestination(
+                icon: const Icon(Icons.dashboard_outlined),
+                label: Text(AppLocalizations.of(context)!.dashboard),
+              ),
+              NavigationRailDestination(
+                icon: const Icon(Icons.wallet_outlined),
+                label: Text(AppLocalizations.of(context)!.details),
+              ),
+              NavigationRailDestination(
+                icon: const Icon(Icons.account_balance_outlined),
+                label: Text(AppLocalizations.of(context)!.accounts),
+              ),
+              NavigationRailDestination(
+                icon: const Icon(Icons.report_outlined),
+                label: Text(AppLocalizations.of(context)!.reports),
+              ),
+            ],
+            onDestinationSelected: (value) {
+              context.read<HomeCubit>().changeCurrentIndex(value);
+            },
+            selectedIndex: context.watch<HomeCubit>().state.currentIndex,
+            trailing: Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: IconButton(
+                  onPressed: () {
+                    context.read<HomeCubit>().changeExpandedRail();
+                  },
+                  icon: Icon(
+                    context.watch<HomeCubit>().state.extendedRail
+                        ? Icons.arrow_back_ios_new_outlined
+                        : Icons.arrow_forward_ios_outlined,
+                  ),
+                ),
+              ),
+            ),
+            labelType: context.watch<HomeCubit>().state.extendedRail
+                ? null
+                : NavigationRailLabelType.selected,
+            minExtendedWidth: 200.0,
+          ),
+          const VerticalDivider(thickness: 1, width: 1),
+          Expanded(child: Container()),
+        ],
+      ),
+    );
   }
 }
