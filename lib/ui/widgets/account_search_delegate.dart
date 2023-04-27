5645c0e26b26900ce8aa8998008c3ddfd180bd2e
Alex Astudillo <astudilloalex1@gmail.com>
2023-04-26 22:06:16 -0500

    Add Account search delegate

--- lib/ui/widgets/account_search_delegate.dart
+++ lib/ui/widgets/account_search_delegate.dart
@@ -1,1 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:simple_accounting_offline/src/account/domain/account.dart';
 
+class AccountSearchDelegate extends SearchDelegate<Account?> {
+  AccountSearchDelegate({
+    this.data = const [],
+  });
+
+  final List<Account> data;
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () => query = '',
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    final List<Account> filter = data.where((element) {
+      return element.code.toLowerCase().contains(query.toLowerCase()) ||
+          element.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    return ListView.builder(
+      itemCount: filter.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(filter[index].name),
+          trailing: Text(filter[index].code),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final List<Account> filter = data.where((element) {
+      return element.code.toLowerCase().contains(query.toLowerCase()) ||
+          element.name.toLowerCase().contains(query.toLowerCase());
+    }).toList();
+    return ListView.builder(
+      itemCount: filter.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(filter[index].name),
+          trailing: Text(filter[index].code),
+        );
+      },
+    );
+  }
+}
