157390cca1f0682b8417a297680ee1fba2cd2b5e
Alex Astudillo <astudilloalex1@gmail.com>
2023-04-28 20:20:50 -0500

    Create seat detail entity

---
+++ lib/src/seat_detail/domain/seat_detail.dart
@@ -0,0 +1,74 @@
+class SeatDetail {
+  const SeatDetail({
+    required this.accountId,
+    this.code = '',
+    this.credit = 0.0,
+    this.debit = 0.0,
+    this.description,
+    this.documentNumber,
+    this.documentType,
+    this.id,
+    this.seatId = 0,
+  });
+
+  final int accountId;
+  final String code;
+  final double credit;
+  final double debit;
+  final String? description;
+  final String? documentNumber;
+  final String? documentType;
+  final int? id;
+  final int seatId;
+
+  SeatDetail copyWith({
+    int? accountId,
+    String? code,
+    double? credit,
+    double? debit,
+    String? description,
+    String? documentNumber,
+    String? documentType,
+    int? id,
+    int? seatId,
+  }) {
+    return SeatDetail(
+      accountId: accountId ?? this.accountId,
+      code: code ?? this.code,
+      credit: credit ?? this.credit,
+      debit: debit ?? this.debit,
+      description: description ?? this.description,
+      documentNumber: documentNumber ?? this.documentNumber,
+      documentType: documentType ?? this.documentType,
+      id: id ?? this.id,
+      seatId: seatId ?? this.seatId,
+    );
+  }
+
+  factory SeatDetail.fromSQLite(Map<String, Object?> map) {
+    return SeatDetail(
+      accountId: map['account_id']! as int,
+      code: map['code']! as String,
+      credit: map['credit']! as double,
+      debit: map['debit']! as double,
+      description: map['description'] as String?,
+      documentNumber: map['document_number'] as String?,
+      documentType: map['document_type'] as String?,
+      id: map['id']! as int,
+      seatId: map['seat_id']! as int,
+    );
+  }
+
+  Map<String, Object?> toSQLite() {
+    return {
+      'account_id': accountId,
+      'code': code,
+      'credit': credit,
+      'debit': debit,
+      'description': description,
+      'document_number': documentNumber,
+      'document_type': documentType,
+      'seat_id': seatId,
+    };
+  }
+}
