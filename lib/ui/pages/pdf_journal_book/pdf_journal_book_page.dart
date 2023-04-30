b742abf58e62895e97f4cfc11fc2c4f6d8d44c57
Alex Astudillo <astudilloalex1@gmail.com>
2023-04-30 00:01:53 -0500

    Add PDF income statement

--- lib/ui/pages/pdf_journal_book/pdf_journal_book_page.dart
+++ lib/ui/pages/pdf_journal_book/pdf_journal_book_page.dart
@@ -27,34 +27,33 @@ class PDFJournalBookPage extends StatelessWidget {
         build: (_) => context.read<PDFJournalBookCubit>().getDoc(),
         maxPageWidth: 1000.0,
         actions: [
-          if (!context.watch<PDFJournalBookCubit>().state.downloading)
-            IconButton(
-              iconSize: 20,
-              onPressed: () async {
-                const String fileName = 'journal_book';
-                final Uint8List? data =
-                    context.read<PDFJournalBookCubit>().state.file;
-                final String? path = await getSavePath(suggestedName: fileName);
-                if (path == null || data == null) return;
-                final XFile file = XFile.fromData(
-                  data,
-                  mimeType: 'application/pdf',
-                  name: fileName,
-                );
-                if (context.mounted) {
-                  context.read<PDFJournalBookCubit>().changeDownloading(
-                        downloading: true,
-                      );
-                }
-                await file.saveTo('$path.pdf');
-                if (context.mounted) {
-                  context.read<PDFJournalBookCubit>().changeDownloading(
-                        downloading: false,
-                      );
-                }
-              },
-              icon: const Icon(Icons.download_outlined),
-            ),
+          IconButton(
+            iconSize: 20,
+            onPressed: () async {
+              const String fileName = 'journal_book';
+              final Uint8List? data =
+                  context.read<PDFJournalBookCubit>().state.file;
+              final String? path = await getSavePath(suggestedName: fileName);
+              if (path == null || data == null) return;
+              final XFile file = XFile.fromData(
+                data,
+                mimeType: 'application/pdf',
+                name: fileName,
+              );
+              if (context.mounted) {
+                context.read<PDFJournalBookCubit>().changeDownloading(
+                      downloading: true,
+                    );
+              }
+              await file.saveTo('$path.pdf');
+              if (context.mounted) {
+                context.read<PDFJournalBookCubit>().changeDownloading(
+                      downloading: false,
+                    );
+              }
+            },
+            icon: const Icon(Icons.download_outlined),
+          ),
         ],
       ),
     );
